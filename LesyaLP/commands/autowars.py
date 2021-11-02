2029be0698ed3fe311d2ccdf50b8c15a22562e3c
g4g4mer <78439764+g4g4mer@users.noreply.github.com>
2021-11-02 12:12:59 +0300

    Update autowars.py

--- LesyaLP/commands/autowars.py
+++ LesyaLP/commands/autowars.py
@@ -3,6 +3,8 @@ from vkbottle.user import Blueprint, Message
 from vkbottle.rule import PrivateMessage, FromMe
 import re
 from .utils import edit_message, prefix
+from random import randint
+from asyncio import sleep
 
 war = False
 
@@ -27,12 +29,15 @@ async def war_False(message: Message, **kwargs):
 @user.on.message_handler(PrivateMessage())
 async def handler(message: Message):
 	if war == True:
+		t = randint(13, 48)
 		a = re.search('Ваши питомцы проиграли', message.text)
 		b = re.search('Ваши питомцы победили!', message.text)
 			
 		if b != None:
+			sleep(t)
 			await user.api.messages.send(random_id=0, peer_id=message.peer_id, message='бой')
 		if a != None:
+			sleep(t)
 			await user.api.messages.send(random_id=0, peer_id=message.peer_id, message='бой')
 	elif war == False:
 		pass
